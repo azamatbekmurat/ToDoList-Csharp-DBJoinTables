3cb93b054315c47535e082dfb9e7cf370d2f827d
Guest User <Guest@epicodus-27.local>
2018-05-02 10:04:26 -0700

    add saving and finding objects methods to project directory, and methods to test those methods

--- ToDoList.Tests/ModelTests/ItemTest.cs
+++ ToDoList.Tests/ModelTests/ItemTest.cs
@@ -1,55 +1,52 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
 using ToDoList.Models;
 using System;
 
 namespace ToDoList.Tests
 {
-  [TestClass]
-  public class ItemTest : IDisposable
-  {
-    [TestMethod]
-    public void GetDescription_ReturnsDescription_String()
+
+    [TestClass]
+    public class ItemTests : IDisposable
     {
-      //Arrange
-      string description = "Walk the dog.";
-      Item newItem = new Item(description);
-      newItem.Save(); //there is no Save() method on project directory since Category class was added.
+        public void Dispose()
+        {
+            Item.DeleteAll();
+        }
+        public ItemTests()
+        {
+            DBConfiguration.ConnectionString = "server=localhost;user id=root;password=root;port=8889;database=todo_test;";
+        }
 
-      //Act
-      string result = newItem.GetDescription();
+        [TestMethod]
+        public void Save_AssignsIdToObject_Id()
+        {
+          //Arrange
+          Item testItem = new Item("Mow the lawn");
 
-      //Assert
-      Assert.AreEqual(description, result);
+          //Act
+          testItem.Save();
+          Item savedItem = Item.GetAll()[0];
+
+          int result = savedItem.GetId();
+          int testId = testItem.GetId();
+
+          //Assert
+          Assert.AreEqual(testId, result);
+        }
+
+        [TestMethod]
+        public void Find_FindsItemInDatabase_Item()
+        {
+          //Arrange
+          Item testItem = new Item("Mow the lawn");
+          testItem.Save();
+
+          //Act
+          Item foundItem = Item.Find(testItem.GetId());
+
+          //Assert
+          Assert.AreEqual(testItem, foundItem);
+        }
     }
-
-    //to clear Item before starting next test case
-    public void Dispose()
-    {
-      Item.ClearAll();
-    }
-
-    [TestMethod]
-    public void GetAll_ReturnsItems_ItemList()
-    {
-      //Arrange
-      string description01 = "Walk the dog";
-      string description02 = "Wash the dishes";
-      Item newItem1 = new Item(description01);
-      newItem1.Save(); //there is no Save() method on project directory since Category class was added.
-      Item newItem2 = new Item(description02);
-      newItem2.Save(); //there is no Save() method on project directory since Category class was added.
-      List<Item> newList = new List<Item> { newItem1, newItem2 };
-
-      //Act
-      List<Item> result = Item.GetAll();
-
-      // foreach (Item thisItem in result)
-      // {
-      //   Console.WriteLine("Output: " + thisItem.GetDescription());
-      // }
-
-      //Assert
-      CollectionAssert.AreEqual(newList, result);
-    }
-  }
 }
