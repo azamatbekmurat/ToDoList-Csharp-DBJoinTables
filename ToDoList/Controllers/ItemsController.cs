3b1e8a4e039b1705d1d5f08de8cbcb50cab7d3f8
Guest User <Guest@epicodus-27.local>
2018-05-02 15:24:29 -0700

    fix subrepo problem

---
+++ ToDoList/Controllers/ItemsController.cs
@@ -0,0 +1,61 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using ToDoList.Models;
+
+namespace ToDoList.Controllers
+{
+    public class ItemsController : Controller
+    {
+
+        [HttpGet("/items")]
+        public ActionResult Index()
+        {
+            List<Item> allItems = Item.GetAll();
+            return View(allItems);
+        }
+
+        [HttpGet("/items/new")]
+        public ActionResult CreateForm()
+        {
+            return View();
+        }
+        [HttpPost("/items")]
+        public ActionResult Create()
+        {
+          Item newItem = new Item (Request.Form["new-item"], 0);
+          newItem.Save();
+          List<Item> allItems = Item.GetAll();
+          return RedirectToAction("Index");
+        }
+
+        [HttpGet("/items/{id}")]
+        public ActionResult Details(int id)
+        {
+            Item item = Item.Find(id);
+            return View(item);
+        }
+
+        [HttpGet("/items/{id}/update")]
+        public ActionResult UpdateForm(int id)
+        {
+            Item thisItem = Item.Find(id);
+            return View(thisItem);
+        }
+
+        // [HttpPost("/items/{id}/update")]
+        // public ActionResult Update(int id)
+        // {
+        //     Item thisItem = Item.Find(id);
+        //     thisItem.Edit(Request.Form["newname"]);
+        //     return RedirectToAction("Index");
+        // }
+
+        [HttpPost("/items/delete")]
+        public ActionResult DeleteAll()
+        {
+            Item.DeleteAll();
+            return View();
+        }
+
+    }
+}
